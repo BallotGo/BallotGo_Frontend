7a10aee1f6864a87f9a4bdfc676e45a3ab2e5752
sanujaediri <sanujaedirisinghe@gmail.com>
2024-10-24 20:48:38 +0530

    repo imports (by Zury7)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LoginReg from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>BallotGo</h1>
-      <p>BallotGo admin dashboard</p>
+        <h1>BallotGo</h1>
+        <p>BallotGo Dashboard</p>
+        <LoginReg />
       </header>
     </div>
   );
